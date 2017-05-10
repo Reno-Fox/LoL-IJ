f1d40826e548aa9ae420aeac9e07d941ef24a143
BlacksheepNZ <matthewgraystone1@gmail.com>
2017-05-11 01:04:04 +1000

    player experience

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public float totalXp;
+    public int currentLevel;
+    public int xpTillNextLevel;
+
+    void Update()
+    {
+        UpdateXp(1);
+    }
+
+    void UpdateXp(float experience)
+    {
+        totalXp = totalXp + experience;
+        int currentXp = (int)(0.1f * Math.Sqrt(totalXp));
+
+        if(currentXp != currentLevel)
+        {
+            currentLevel = currentXp;
+            Debug.Log("Leveled up.");
+        }
+
+        xpTillNextLevel = 100 * (currentLevel + 1) * (currentLevel + 1);
+    }
+}
